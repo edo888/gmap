d261c3ac6e83add27b330168be265f9591cda215
edo888 <edo888@gmail.com>
2015-04-06 13:51:00 -0400

    Removed unnecessary bootstrap JHTML library

--- com_gmap/admin/views/maps/tmpl/default.php
+++ com_gmap/admin/views/maps/tmpl/default.php
@@ -11,7 +11,6 @@
 // no direct access
 defined('_JEXEC') or die('Restricted access');
 
-JHtml::_('bootstrap.tooltip');
 JHtml::_('behavior.multiselect');
 JHtml::_('dropdown.init');
 JHtml::_('formbehavior.chosen', 'select');
